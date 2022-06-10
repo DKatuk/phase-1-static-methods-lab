26fc9382cac56f893b5b6db6a32a72327c1492cb
Dilara Katuk <dilarakatuk@gmail.com>
2022-06-11 00:39:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(str) {
+    const lower = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from'];
+    const words = str.split(' ');
+    const result = [];
+
+   for(let i = 0; i < words.length; i++) {
+    if (i == 0) {
+      result.push(this.capitalize(words[i]));
+    } else {
+      if (lower.includes(words[i])) {
+        result.push(words[i]);
+      } else {
+        result.push(this.capitalize(words[i]));
+      }
+    }
+  } return result.join(" ");
+
+   }
 }
+
+  
